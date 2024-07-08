9fdaeb8f13a98c018e1d3ec146cb0f5f6d8c24bd
wincenzo <wincenzo@protonmail.com>
2024-07-08 10:40:10 +0200

    ok

--- model/ease_jax.py
+++ model/ease_jax.py
@@ -2,10 +2,10 @@ from functools import partial
 from typing import Optional, Self, Sequence
 
 import jax.numpy as jnp
+import jax.scipy as jsp
 import numpy as np
 import pandas as pd
 from jax import Array, device_get, jit, lax, vmap
-from jax.scipy.linalg import solve
 from scipy.sparse import csr_matrix
 from sklearn.preprocessing import LabelEncoder, MaxAbsScaler
 
@@ -23,15 +23,14 @@ class EASE:
 
         self.implicit = not score_col
 
-        self.users = self.user_enc.fit_transform(
-            train[user_col].to_numpy())
-        self.items = self.item_enc.fit_transform(
-            train[item_col].to_numpy())
+        self.users = self.user_enc.fit_transform(train[user_col].to_numpy())
+        self.items = self.item_enc.fit_transform(train[item_col].to_numpy())
 
         values = (
-            np.ones(self.users.size, dtype=bool)  # type: ignore
+            jnp.ones(self.users.size, dtype=bool)  # type: ignore
             if self.implicit
-            else self.values_enc.fit_transform(train[score_col].to_numpy())
+            else jnp.asarray(self.values_enc.fit_transform(
+                train[score_col].to_numpy()), dtype=jnp.float32)
         )
 
         self.n_items = self.item_enc.classes_.size
@@ -49,27 +48,33 @@ class EASE:
     def _compute_B(G: Array, lambda_: float) -> Array:
         diag_indices = jnp.diag_indices(G.shape[0])
         G = G.at[diag_indices].add(lambda_)
-        P = solve(G, jnp.eye(G.shape[0]))
+        P = jsp.linalg.solve(G, jnp.eye(G.shape[0]))
         B = P / (-jnp.diag(P))[:, None]
         B = B.at[diag_indices].set(0)
 
         return B
-    
+
     def fit(self, lambda_: float = 0.5) -> None:
         user_item = self.user_item.astype(np.float32)
         G = user_item.T.dot(user_item).toarray()
+        G = jnp.asarray(G)
         B = self._compute_B(G, lambda_)
         self.predictions = jnp.asarray(self.user_item.dot(B))
 
     @staticmethod
     @partial(jit, static_argnums=(2,))
-    def _predict_single_user(user_prediction: Array, user_mask: Array, k: int) -> tuple[Array, Array]:
+    def _predict_single_user(user_prediction: Array,
+                             user_mask: Array,
+                             k: int,
+                             ) -> tuple[Array, Array]:
         candidates = jnp.where(user_mask, -jnp.inf, user_prediction)
 
         return lax.top_k(candidates, k)
 
     @partial(jit, static_argnums=(0,))
-    def _predict(self, users_idx: Array, users_user_item: Array) -> tuple[Array, Array]:
+    def _predict(self, users_idx: Array,
+                 users_user_item: Array,
+                 ) -> tuple[Array, Array]:
         users_prediction = self.predictions[users_idx]
         vectorized_predict = vmap(
             self._predict_single_user, in_axes=(0, 0, None))
@@ -81,11 +86,11 @@ class EASE:
         self.users_pred = users_pred
         users_pred_idx = jnp.asarray(
             self.user_enc.transform(users_pred), dtype=jnp.uint32)
-        users_user_item = jnp.asarray(
+        user_item_pred = jnp.asarray(
             self.user_item[users_pred_idx].toarray(), dtype=jnp.bool)
 
         self.top_k_scores, top_k_indices = self._predict(
-            users_pred_idx, users_user_item)
+            users_pred_idx, user_item_pred)
         self.top_k_results = self.item_enc.inverse_transform(
             device_get(top_k_indices).ravel())
 
@@ -101,6 +106,3 @@ class EASE:
         )
 
         return results
-
-
-
